bc8b5aba1f2b97231b72a0ca8384de76f76c2835
marco <toyko2001@gmail.com>
2024-05-23 11:49:24 +0630

    test(ctx): test content encrypy/decryot

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@ mod error;
 mod utils;
 
 fn main() -> Result<(), error::Error> {
-    let encryption = encryptor::Encryption::new();
-
-    let password_manager = content::ContentManager::new("fish");
-
-    let encrypted = encryption.encrypt(&password_manager)?;
-    let decrypted = encryption.decrypt(&password_manager, &encrypted)?;
-
-    assert_eq!(
-        decrypted,
-        password_manager.password.as_bytes(),
-        "same bytes"
-    );
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encrypt_decrypt_content() {
+        let encryption = encryptor::Encryption::new();
+
+        let content = content::ContentManager::new("fish");
+
+        let encrypted = encryption
+            .encrypt(&content)
+            .expect("Unable to encrypt content");
+        let decrypted = encryption
+            .decrypt(&content, &encrypted)
+            .expect("Unable to decrypt to content");
+
+        assert_eq!(content.ctx.as_bytes(), decrypted);
+    }
 }
